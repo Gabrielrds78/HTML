7516345a7876c7f0238361a6c6cc3a0ea9dc8824
gabriel.rds <100387568+Gabrielrds78@users.noreply.github.com>
2023-04-10 21:42:47 -0300

    Add files via upload

---
+++ metalcoinExercicio/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8"/>
+<link rel="stylesheet" href="css/login.css">
+<title>Login</title>
+</head>
+<body>
+    <div><p class="logo">FootShop</p></div>
+    <form class="tela" action="logar.php" method="post">
+        <label class="textlogin" for="">Login</label>
+        <input class="login" type="text" name="login" id="">
+        <br>
+        <label for="">Senha</label>
+        <input type="password" name="senha" id="">
+        <button>enviar</button>
+    </form>
+</body>
+</html>
